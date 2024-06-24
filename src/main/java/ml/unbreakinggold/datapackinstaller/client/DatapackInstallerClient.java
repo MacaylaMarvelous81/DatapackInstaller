0527af3425405884fa68ddc674907be96335fb2c
Jomar Milan <jomarm@jomarm.com>
2024-06-23 23:22:04 -0700

    Add multiple search paths

--- src/main/java/ml/unbreakinggold/datapackinstaller/client/DatapackInstallerClient.java
+++ src/main/java/ml/unbreakinggold/datapackinstaller/client/DatapackInstallerClient.java
@@ -4,17 +4,23 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
+import java.io.File;
 import java.nio.file.Path;
 
 @Environment(EnvType.CLIENT)
 public class DatapackInstallerClient implements ClientModInitializer {
-    public static Path globalDatapackPath;
+    private static final Logger LOGGER = LogManager.getLogger(DatapackInstallerClient.class);
+    private static final Path MAIN_PATH = FabricLoader.getInstance().getGameDir().resolve("datapacks");
+    private static final Path LEGACY_PATH = FabricLoader.getInstance().getGameDir().resolve("installed_datapacks");
+    public static Path[] SearchPaths = new Path[]{ MAIN_PATH, LEGACY_PATH };
+
     @Override
     public void onInitializeClient() {
-        globalDatapackPath = FabricLoader.getInstance().getGameDir().resolve("installed_datapacks");
-        if (!globalDatapackPath.toFile().exists()) {
-            globalDatapackPath.toFile().mkdirs();
-        }
+        File mainFile = MAIN_PATH.toFile();
+        if (!mainFile.exists() && !mainFile.mkdirs())
+            LOGGER.warn("User intervention recommended: default search path 'datapacks' could not be created.");
     }
 }
