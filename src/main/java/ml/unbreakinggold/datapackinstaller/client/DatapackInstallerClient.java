c81a0ab4565a7ec5fc337d24e070aaf48a978d02
Jomar Milan <jomarm@jomarm.com>
2024-07-31 22:02:14 -0700

    Migrate from installed_datapacks to datapacks fully

--- src/main/java/ml/unbreakinggold/datapackinstaller/client/DatapackInstallerClient.java
+++ src/main/java/ml/unbreakinggold/datapackinstaller/client/DatapackInstallerClient.java
@@ -4,12 +4,13 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.loader.api.FabricLoader;
+import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.File;
+import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 
 @Environment(EnvType.CLIENT)
 public class DatapackInstallerClient implements ClientModInitializer {
@@ -20,7 +21,17 @@ public class DatapackInstallerClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         File mainFile = MAIN_PATH.toFile();
-        if (!mainFile.exists() && !mainFile.mkdirs())
-            LOGGER.warn("User intervention recommended: default search path 'datapacks' could not be created.");
+        File legacyFile = LEGACY_PATH.toFile();
+        if (!mainFile.exists() && !legacyFile.exists() && legacyFile.isDirectory() && !mainFile.mkdirs())
+            LOGGER.warn("User intervention recommended: default search path 'datapacks' could not be created. Please create it manually.");
+
+        if (legacyFile.exists() && legacyFile.isDirectory()) {
+            try {
+                FileUtils.moveDirectory(legacyFile, mainFile);
+                LOGGER.info("Migrated from installed_datapacks to datapacks.");
+            } catch (IOException e) {
+                LOGGER.error("User intervention recommended: Failed to migrate from installed_datapacks to datapacks. Please move datapacks manually.", e);
+            }
+        }
     }
 }
