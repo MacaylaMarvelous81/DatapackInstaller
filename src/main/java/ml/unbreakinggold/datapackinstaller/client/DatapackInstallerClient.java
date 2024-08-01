f1014d2654b5af93fca6d47a422adcc8d036a7bf
Jomar Milan <jomarm@jomarm.com>
2024-07-31 21:03:18 -0700

    Replace temporary directory with persistent directory

--- src/main/java/ml/unbreakinggold/datapackinstaller/client/DatapackInstallerClient.java
+++ src/main/java/ml/unbreakinggold/datapackinstaller/client/DatapackInstallerClient.java
@@ -9,13 +9,13 @@ import org.apache.logging.log4j.Logger;
 
 import java.io.File;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 
 @Environment(EnvType.CLIENT)
 public class DatapackInstallerClient implements ClientModInitializer {
     private static final Logger LOGGER = LogManager.getLogger(DatapackInstallerClient.class);
-    private static final Path MAIN_PATH = FabricLoader.getInstance().getGameDir().resolve("datapacks");
-    private static final Path LEGACY_PATH = FabricLoader.getInstance().getGameDir().resolve("installed_datapacks");
-    public static Path[] SearchPaths = new Path[]{ MAIN_PATH, LEGACY_PATH };
+    public static final Path MAIN_PATH = FabricLoader.getInstance().getGameDir().resolve("datapacks");
+    public static final Path LEGACY_PATH = FabricLoader.getInstance().getGameDir().resolve("installed_datapacks");
 
     @Override
     public void onInitializeClient() {
--- src/main/java/ml/unbreakinggold/datapackinstaller/mixin/CreateWorldScreenMixin.java
+++ src/main/java/ml/unbreakinggold/datapackinstaller/mixin/CreateWorldScreenMixin.java
@@ -2,39 +2,42 @@ package ml.unbreakinggold.datapackinstaller.mixin;
 
 import ml.unbreakinggold.datapackinstaller.client.DatapackInstallerClient;
 import net.minecraft.client.gui.screen.world.CreateWorldScreen;
-import org.apache.commons.io.FileUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Unique;
 
-import java.io.File;
-import java.io.IOException;
 import java.nio.file.Path;
 
 @Mixin(CreateWorldScreen.class)
 public abstract class CreateWorldScreenMixin {
+    @Unique
     private static final Logger LOGGER = LogManager.getLogger(CreateWorldScreenMixin.class);
-    @Shadow @Nullable protected abstract Path getDataPackTempDir();
+    @Shadow
+    @Nullable
+    private Path dataPackTempDir;
 
-    @Inject(at = @At("HEAD"), method = "openPackScreen")
-    private void init(CallbackInfo info) {
-        Path tempPath = this.getDataPackTempDir();
-        File tempPathFile = tempPath.toFile();
+    /**
+     * @author Jomar Milan - July 31st, 2024 - Minecraft 1.20.5
+     * @reason Use persistent mod directory instead of vanilla temporary directory
+     */
+    @Overwrite
+    @Nullable
+    private Path getDataPackTempDir() {
+        this.dataPackTempDir = DatapackInstallerClient.MAIN_PATH;
 
-        for (Path searchPath : DatapackInstallerClient.SearchPaths) {
-            File searchPathFile = searchPath.toFile();
-            if (!searchPathFile.exists()) continue;
+        return this.dataPackTempDir;
+    }
 
-            try {
-                FileUtils.copyDirectory(searchPathFile, tempPathFile);
-            } catch (IOException e) {
-                LOGGER.error("Could not copy from search path {}. Message: {}", searchPath, e.getMessage());
-            }
-        }
+    /**
+     * @author Jomar Milan - July 31st, 2024 - Minecraft 1.20.5
+     * @reason Because dataPackTempDir is now persistent and not temporary, the directory should no longer be cleared.
+     */
+    @Overwrite
+    private void clearDataPackTempDir() {
+        LOGGER.info("Suppressing attempt to clear data pack directory");
     }
 }
