85d7490d326d4e52aaf3f6193cdde7a19807fbd9
Jomar Milan <jomarm@jomarm.com>
2024-08-04 21:44:14 -0700

    Implement server data pack installing

--- src/main/java/ml/unbreakinggold/datapackinstaller/server/DatapackInstallerServer.java
+++ src/main/java/ml/unbreakinggold/datapackinstaller/server/DatapackInstallerServer.java
@@ -3,10 +3,19 @@ package ml.unbreakinggold.datapackinstaller.server;
 import net.fabricmc.api.DedicatedServerModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.loader.api.FabricLoader;
+
+import java.io.File;
+import java.nio.file.Path;
 
 @Environment(EnvType.SERVER)
 public class DatapackInstallerServer implements DedicatedServerModInitializer {
+    public static final Path MAIN_PATH = FabricLoader.getInstance().getGameDir().resolve("datapacks");
+
     @Override
     public void onInitializeServer() {
+        File mainFile = MAIN_PATH.toFile();
+
+        if (!mainFile.exists()) mainFile.mkdirs();
     }
 }
